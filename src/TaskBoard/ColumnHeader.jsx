15bc11234454ffaed054f6820cf497695693643f
shefatmishu <shefatmishu@gmail.com>
2024-10-27 23:05:48 +0600

    Sorting added

--- src/TaskBoard/ColumnHeader.jsx
+++ src/TaskBoard/ColumnHeader.jsx
@@ -1,12 +1,19 @@
 import SortingSvg from "../Svg-Components/SortingSvg";
 
-export default function ColumnHeader({ title, count = 0 }) {
+export default function ColumnHeader({ title, count = 0, handleFilter }) {
   return (
     <div className="mb-2 flex items-center justify-between">
       <h3 className="text-lg font-semibold">
         {title}({count})
       </h3>
-      <SortingSvg />
+      <div
+        onClick={(e) => {
+          e.preventDefault();
+          handleFilter();
+        }}
+      >
+        <SortingSvg />
+      </div>
     </div>
   );
 }
--- src/TaskBoard/TaskColumn.jsx
+++ src/TaskBoard/TaskColumn.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { TaskDispatchContext } from "../context/TaskContext";
 import DeleteSvg from "../Svg-Components/DeleteSvg";
 import EditSvg from "../Svg-Components/EditSvg";
@@ -7,6 +7,7 @@ import NoTasksFound from "./NoTaskFound";
 
 export default function TaskColumn({ title, type, tasks = [], onEdit }) {
   const dispatch = useContext(TaskDispatchContext);
+  const [isAscending, setIsAscending] = useState(false);
 
   const getBgColor = () => {
     if (type === "todo") {
@@ -22,45 +23,62 @@ export default function TaskColumn({ title, type, tasks = [], onEdit }) {
     }
   };
 
+  console.log({ tas: tasks });
+
   return (
     <>
       <div className="mb-4 w-full px-2 sm:w-1/2 md:w-1/4">
         <div className={`rounded-lg ${getBgColor()} p-4`}>
-          <ColumnHeader title={title} count={tasks.length} />
-          {tasks.map((task) => (
-            <>
-              <div>
-                <div className="mb-4 rounded-lg bg-gray-800 p-4">
-                  <div className="flex justify-between">
-                    <h4 className="mb-2 flex-1 font-semibold text-indigo-500">
-                      {task.taskName}
-                    </h4>
-                    <div className="flex gap-2">
-                      <div
-                        onClick={() =>
-                          dispatch({ type: "EDIT_TASK", payload: task })
-                        }
-                      >
-                        <EditSvg />
-                      </div>
-                      <div
-                        onClick={() =>
-                          dispatch({ type: "DELETE_TO_TASK", payload: task.id })
-                        }
-                      >
-                        <DeleteSvg />
+          <ColumnHeader
+            title={title}
+            count={tasks.length}
+            handleFilter={() => {
+              isAscending ? setIsAscending(false) : setIsAscending(true);
+            }}
+          />
+          {tasks
+            .sort((a, b) =>
+              isAscending
+                ? new Date(a.date) - new Date(b.date)
+                : new Date(b.date) - new Date(a.date)
+            )
+            .map((task) => (
+              <>
+                <div>
+                  <div className="mb-4 rounded-lg bg-gray-800 p-4">
+                    <div className="flex justify-between">
+                      <h4 className="mb-2 flex-1 font-semibold text-indigo-500">
+                        {task.taskName}
+                      </h4>
+                      <div className="flex gap-2">
+                        <div
+                          onClick={() =>
+                            dispatch({ type: "EDIT_TASK", payload: task })
+                          }
+                        >
+                          <EditSvg />
+                        </div>
+                        <div
+                          onClick={() =>
+                            dispatch({
+                              type: "DELETE_TO_TASK",
+                              payload: task.id,
+                            })
+                          }
+                        >
+                          <DeleteSvg />
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  <p className="mb-2 text-sm text-zinc-200">
-                    {task.description}
-                  </p>
+                    <p className="mb-2 text-sm text-zinc-200">
+                      {task.description}
+                    </p>
 
-                  <p className="mt-6 text-xs text-zinc-400">{task.date}</p>
+                    <p className="mt-6 text-xs text-zinc-400">{task.date}</p>
+                  </div>
                 </div>
-              </div>
-            </>
-          ))}
+              </>
+            ))}
           {tasks.length === 0 && <NoTasksFound />}
         </div>
       </div>
